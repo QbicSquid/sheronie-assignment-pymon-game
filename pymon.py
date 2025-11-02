d8e26ac51f5b2b2174d585638dd26bd4206d20b7
QbicSquid <shifan611@gmail.com>
2025-11-02 14:14:38 +0530

    formatting

--- pymon.py
+++ pymon.py
@@ -5,7 +5,7 @@ Created on Wed Oct 16 22:29:55 2024
 Pymon skeleton game
 Please make modifications to all the classes to match with requirements provided in the assignment spec document
 @author: dipto
-@student_id : 
+@student_id :
 @highest_level_attempted (P/C/D/HD):
 
 - Reflection:
@@ -15,35 +15,36 @@ Please make modifications to all the classes to match with requirements provided
 import random
 
 
-#you may use, extend and modify the following random generator
-def generate_random_number(max_number = 1):
-    r = random.randint(0,max_number)
-    return r 
+# you may use, extend and modify the following random generator
+def generate_random_number(max_number=1):
+    r = random.randint(0, max_number)
+    return r
 
 
 class Pymon:
-    def __init__(self, name = "The player"):
+    def __init__(self, name="The player"):
         self.name = name
         self.current_location = None
-    
-    def move(self, direction = None):
+
+    def move(self, direction=None):
         if self.current_location != None:
             if self.current_location.doors[direction] != None:
-                self.current_location.doors[direction].add_creature(self)  
+                self.current_location.doors[direction].add_creature(self)
                 self.current_location.creatures.remove(self)
             else:
                 print("no access to " + direction)
-                
+
     def spawn(self, loc):
         if loc != None:
             loc.add_creature(self)
             self.current_location = loc
-            
+
     def get_location(self):
         return self.current_location
-            
+
+
 class Location:
-    def __init__(self, name = "New room", w = None, n = None , e = None, s = None):
+    def __init__(self, name="New room", w=None, n=None, e=None, s=None):
         self.name = name
         self.doors = {}
         self.doors["west"] = w
@@ -52,72 +53,73 @@ class Location:
         self.doors["south"] = s
         self.creatures = []
         self.items = []
-        
+
     def add_creature(self, creature):
         pass
-        #please implement this method to by simply appending a creature to self.creatures list.
-        
+        # please implement this method to by simply appending a creature to self.creatures list.
+
     def add_item(self, item):
         pass
-        #please implement this method to by simply appending an item to self.items list.
-        
+        # please implement this method to by simply appending an item to self.items list.
+
     def connect_east(self, another_room):
-        self.doors["east"] = another_room 
-        another_room.doors["west"]  = self;
-        
+        self.doors["east"] = another_room
+        another_room.doors["west"] = self
+
     def connect_west(self, another_room):
-        self.doors["west"] = another_room 
-        another_room.doors["east"]  = self;
-    
+        self.doors["west"] = another_room
+        another_room.doors["east"] = self
+
     def connect_north(self, another_room):
-        self.doors["north"] = another_room 
-        another_room.doors["south"]  = self;
-        
+        self.doors["north"] = another_room
+        another_room.doors["south"] = self
+
     def connect_south(self, another_room):
-        self.doors["south"] = another_room 
-        another_room.doors["north"]  = self;
-        
+        self.doors["south"] = another_room
+        another_room.doors["north"] = self
+
     def get_name(self):
         return self.name
-        
+
+
 class Record:
     def __init__(self):
         self.locations = []
-        #please implement constructor
+        # please implement constructor
 
     def import_location(self):
-        #please import data from locations.csv
-        #here are sample data to start with
+        # please import data from locations.csv
+        # here are sample data to start with
         school = Location("school")
         car_park = Location("car park")
         self.locations.append(school)
         self.locations.append(car_park)
-        
+
         school.connect_west(car_park)
-        
+
     def get_locations(self):
         return self.locations
-        
+
     def import_creatures(self):
-        pass #please import data from creatures.csv
+        pass  # please import data from creatures.csv
 
     def import_items(self):
-        pass #please import data from items.csv
-    
+        pass  # please import data from items.csv
+
+
 class Operation:
-    
+
     def handle_menu(self):
         print("Please issue a command to your Pymon:")
         print("1) Inspect Pymon")
         print("2) Inspect current location")
         print("3) Move")
         print("4) Exit the program")
-        
-    
+
     def __init__(self):
         self.locations = []
         self.current_pymon = Pymon("Kimimon")
-      
+
     def setup(self):
         record = Record()
 
@@ -125,8 +127,8 @@ class Operation:
         for location in record.get_locations():
             self.locations.append(location)
 
-        a_random_number = generate_random_number(len(self.locations)-1)
-        
+        a_random_number = generate_random_number(len(self.locations) - 1)
+
         spawned_loc = self.locations[a_random_number]
 
         self.current_pymon.spawn(spawned_loc)
@@ -137,20 +139,23 @@ class Operation:
             for creature in location.creatures:
                 print(creature.name)
 
-    #you may use this test run to help test methods during development
+    # you may use this test run to help test methods during development
     def test_run(self):
         print(self.current_pymon.get_location().get_name())
         self.current_pymon.move("west")
         print(self.current_pymon.get_location().get_name())
-        
+
     def start_game(self):
         print("Welcome to Pymon World\n")
-        print("It's just you and your loyal Pymon roaming around to find more Pymons to capture and adopt.\n")
-        print("You started at ",self.current_pymon.get_location().get_name())
+        print(
+            "It's just you and your loyal Pymon roaming around to find more Pymons to capture and adopt.\n"
+        )
+        print("You started at ", self.current_pymon.get_location().get_name())
         self.handle_menu()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     ops = Operation()
     ops.setup()
-    #ops.display_setup()
+    # ops.display_setup()
     ops.start_game()
